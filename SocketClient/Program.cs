27874769973fe4351196be0fa3d71e8268791885
Bo Christian Skjøtt <bo@skjoett.com>
2018-12-07 22:26:45 +0100

    First commit

---
+++ SocketClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int messageCounter = 1;
+            int secondsBeforeNextMessage = 5;
+            TcpClient tcpClient = new TcpClient();
+
+            string hostname = Dns.GetHostName();
+            Console.WriteLine($"Socket client started on '{hostname}'");
+
+            string serverAddress = Environment.GetEnvironmentVariable("ServerAddress");
+            string serverPort = Environment.GetEnvironmentVariable("ServerPort");
+            int port = int.Parse(serverPort);
+
+            try
+            {
+                Console.WriteLine($"Connecting to {serverAddress}:{serverPort}");
+                tcpClient.Connect(serverAddress, port);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"tcpClient.Connect exception: {ex.Message}");
+                return;
+            }
+
+            NetworkStream networkStream = tcpClient.GetStream();
+            StreamReader clientStreamReader = new StreamReader(networkStream);
+            StreamWriter clientStreamWriter = new StreamWriter(networkStream);
+
+            while (true)
+            {
+                try
+                {
+                    string message = $"Hello from {hostname} at {DateTime.Now}";
+
+                    Console.WriteLine($"Sending message #{messageCounter++}: '{message}'");
+                    clientStreamWriter.WriteLine(message);
+                    clientStreamWriter.Flush();
+
+                    Console.WriteLine("Waiting for reply...");
+
+                    do
+                    {
+                        message = clientStreamReader.ReadLine();
+                    } while (string.IsNullOrEmpty(message));
+
+                    Console.WriteLine($"Message received at {DateTime.Now}: '{message}'");
+
+                    secondsBeforeNextMessage = 2 * secondsBeforeNextMessage;
+                    DateTime timeNow = DateTime.Now;
+
+                    Console.WriteLine($"Sending next message in {secondsBeforeNextMessage} seconds at {timeNow.AddSeconds(secondsBeforeNextMessage)}");
+                    Thread.Sleep(secondsBeforeNextMessage * 1000);
+                    Console.WriteLine("");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n>>>>>>>> Exception: {ex.Message}\n");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static int messageCounter = 1;
+        static List<TcpClient> connectedClients = new List<TcpClient>();
+
+        static void Main(string[] args)
+        {
+            int port = 5000;
+            string hostname = Dns.GetHostName();
+            Console.WriteLine($"Socket server started on '{hostname}'");
+
+            string serverPort = Environment.GetEnvironmentVariable("ServerPort");
+            if (!string.IsNullOrEmpty(serverPort))
+            {
+                port = int.Parse(serverPort);
+            }
+
+            try
+            {
+                IPEndPoint local = new IPEndPoint(IPAddress.Any, port);
+                TcpListener listener = new TcpListener(local);
+
+                listener.Start();
+                Console.WriteLine($"Listening on port {port}");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Client connected");
+
+                    connectedClients.Add(client);
+                    HandleClient(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"tcpClient.Connect exception:{ex.Message}");
+                return;
+            }
+        }
+
+        private static void HandleClient(TcpClient tcpClient)
+        {
+            string message;
+            NetworkStream networkStream = tcpClient.GetStream();
+            StreamReader clientStreamReader = new StreamReader(networkStream);
+            StreamWriter clientStreamWriter = new StreamWriter(networkStream);
+
+            while (true)
+            {
+                try
+                {
+                    do
+                    {
+                        message = clientStreamReader.ReadLine();
+                    } while (string.IsNullOrEmpty(message));
+
+                    Console.WriteLine($"Echoing message #{messageCounter++} received from client {connectedClients.IndexOf(tcpClient) + 1} at {DateTime.Now}: '{message}'");
+                    message = $"Echo: {message}";
+                    clientStreamWriter.WriteLine(message);
+                    clientStreamWriter.Flush();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n>>>>>>>> Exception: {ex.Message}\n");
+                    break;
+                }
+            }
+
+            clientStreamReader.Dispose();
+            clientStreamWriter.Dispose();
+            networkStream.Dispose();
+        }
+    }
+}
